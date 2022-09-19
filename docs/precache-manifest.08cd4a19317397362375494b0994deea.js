6f0b5a9a838006a149388e10520e3972ae4ba3aa
NookieGrey <gaguliyanugzar@gmail.com>
2022-09-19 14:55:13 +0300

    build 2

---
+++ docs/precache-manifest.08cd4a19317397362375494b0994deea.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a32a287135538e86783cc98ddb5dc611",
+    "url": "/MyAdmin/index.html"
+  },
+  {
+    "revision": "2c5b4a78c966feaaf123",
+    "url": "/MyAdmin/static/css/2.249a6280.chunk.css"
+  },
+  {
+    "revision": "93ca5f549b18d3224854",
+    "url": "/MyAdmin/static/css/4.63c3aedc.chunk.css"
+  },
+  {
+    "revision": "74a0462e6e99fe5579b1",
+    "url": "/MyAdmin/static/css/5.1757c055.chunk.css"
+  },
+  {
+    "revision": "473b82536558b6432067",
+    "url": "/MyAdmin/static/css/6.98f5dbea.chunk.css"
+  },
+  {
+    "revision": "bbc6576b40b0cc5d88c7",
+    "url": "/MyAdmin/static/css/7.ecdda9f0.chunk.css"
+  },
+  {
+    "revision": "e106dcb98b11550d92ef",
+    "url": "/MyAdmin/static/css/8.9b43ac92.chunk.css"
+  },
+  {
+    "revision": "54beecbc537e07ec821a",
+    "url": "/MyAdmin/static/css/9.f794e3d2.chunk.css"
+  },
+  {
+    "revision": "60635f602f67e17d908c",
+    "url": "/MyAdmin/static/css/main.fe1a5efa.chunk.css"
+  },
+  {
+    "revision": "2c5b4a78c966feaaf123",
+    "url": "/MyAdmin/static/js/2.913d858b.chunk.js"
+  },
+  {
+    "revision": "cacaf3f01dd3bd8237006f7c58951c1c",
+    "url": "/MyAdmin/static/js/2.913d858b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66ea53bb222dab0c4756",
+    "url": "/MyAdmin/static/js/3.97a1e5b1.chunk.js"
+  },
+  {
+    "revision": "93ca5f549b18d3224854",
+    "url": "/MyAdmin/static/js/4.fca21be3.chunk.js"
+  },
+  {
+    "revision": "74a0462e6e99fe5579b1",
+    "url": "/MyAdmin/static/js/5.ff1b7daf.chunk.js"
+  },
+  {
+    "revision": "473b82536558b6432067",
+    "url": "/MyAdmin/static/js/6.a4d27eda.chunk.js"
+  },
+  {
+    "revision": "bbc6576b40b0cc5d88c7",
+    "url": "/MyAdmin/static/js/7.4df700f6.chunk.js"
+  },
+  {
+    "revision": "e106dcb98b11550d92ef",
+    "url": "/MyAdmin/static/js/8.7a1483b3.chunk.js"
+  },
+  {
+    "revision": "54beecbc537e07ec821a",
+    "url": "/MyAdmin/static/js/9.5523b6b7.chunk.js"
+  },
+  {
+    "revision": "60635f602f67e17d908c",
+    "url": "/MyAdmin/static/js/main.857430c5.chunk.js"
+  },
+  {
+    "revision": "e55f24bd5014fa232205",
+    "url": "/MyAdmin/static/js/runtime-main.7a61f482.js"
+  },
+  {
+    "revision": "99d2e29697552afc8aaee06150f2aa07",
+    "url": "/MyAdmin/static/media/public_bg_1200_750.99d2e296.jpg"
+  },
+  {
+    "revision": "a620a28ef266bece554b6a7a0359461f",
+    "url": "/MyAdmin/static/media/public_bg_1920_1200.a620a28e.jpg"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6ac26af3ed8d86117a4cfeb8608cb258.js"
+  "/MyAdmin/precache-manifest.08cd4a19317397362375494b0994deea.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/MyAdmin/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
